6c232dbec9f4a839450a146841e46f834dcf62d7
David <zdavid1@myges.fr>
2022-01-23 18:52:03 +0100

    Feature : Header + angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BibliothequeComponent } from './bibliotheque/bibliotheque.component';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTabsModule } from '@angular/material/tabs';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BibliothequeComponent
+    BibliothequeComponent,
+    ConnexionComponent,
+    NavComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+   BrowserModule, FormsModule, AppRoutingModule, MatTabsModule, BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
