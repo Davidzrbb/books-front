75ad1093c99199fb9d4378b4b01a037837cb02aa
David <zdavid1@myges.fr>
2022-01-25 20:39:06 +0100

    Feature : search isbn

--- src/app/bibliotheque/bibliotheque.component.ts
+++ src/app/bibliotheque/bibliotheque.component.ts
@@ -1,25 +1,49 @@
- import { Component, OnInit } from '@angular/core';
-  import {BookServiceService} from '../service/book-service.service'
-
-
-
+import { Component, OnInit } from '@angular/core';
+import {BookServiceService} from '../service/book-service.service'
+import{Livre} from '../bibliotheque/livre'
+import { Observable } from 'rxjs';
+import {MatDialog} from '@angular/material/dialog';
 @Component({
   selector: 'app-bibliotheque',
   templateUrl: './bibliotheque.component.html',
   styleUrls: ['./bibliotheque.component.css']
 })
+
 export class BibliothequeComponent implements OnInit {
-posts : any;
+public posts : any;
+public search:any;
+public popup = false;
+public valSearch="";
 
 
-constructor(private bookServiceService: BookServiceService) { }
+constructor(private bookServiceService: BookServiceService,public dialog: MatDialog) { }
 
  ngOnInit() {
+ this.popup = false;
  	this.bookServiceService.getPosts().subscribe(
- 	(response) => { this.posts = response; },
+ 	(data) => { this.posts = data; },
  	(error) => { console.log(error); });
  }
-
  headElements = ['ID', 'First', 'Last', 'Handle'];
+
+//GET SEARCH ISBN
+ searchIsbn(){
+  const inputValue = this.valSearch;
+  this.bookServiceService.getPostsIsbn(inputValue).subscribe(
+    (data) => { this.search = data;
+    if(this.search.isbn != null){
+        this.popup= true;
+    }else{
+    alert("Isbn don't exist")
+    }
+    },
+    (error) => { console.log(error);
+    console.log("ohoh");
+    });
+
+  }
+    onKey(event: any) {
+      this.valSearch = event.target.value;
+    }
 
 }
--- src/app/service/book-service.service.ts
+++ src/app/service/book-service.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from  '@angular/common/http';
+import{Livre} from '../bibliotheque/livre'
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
 export class BookServiceService {
 
   private url = 'http://localhost:8080/api/book/';
-  constructor(private http: HttpClient) { }
-  getPosts() {
-  	return this.http.get(this.url);
+  constructor(private http: HttpClient){ }
+  getPosts():Observable<any> {
+  	return this.http.get<any>(this.url);
   }
-
+  getPostsIsbn(isbn : string):Observable<any> {
+  	return this.http.get<any>(this.url+isbn);
+  }
 }
 
